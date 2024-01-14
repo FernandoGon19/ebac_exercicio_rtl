531b40145bac0208387bcceee78e52dbe412d87e
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2024-01-14 19:32:33 -0300

    Arquivos Atualizados

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,27 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Teste para inserção de dois comentarios', () => {
+        render(<PostComment/>);
+
+        // adiciona o primeiro comentário
+        fireEvent.change(screen.getByTestId('ComentarioTextArea'), {
+            target: {
+                value: 'Teste 1',
+            }
+        });
+        fireEvent.click(screen.getByTestId('ComentarioBotao'));
+    
+        // adiciona o segundo comentário
+        fireEvent.change(screen.getByTestId('ComentarioTextArea'), {
+            target: {
+                value: 'Teste 2',
+            }
+        });
+        fireEvent.click(screen.getByTestId('ComentarioBotao'));
+
+        expect(screen.getAllByTestId('ComentarioElemento')).toHaveLength(2);
+
+    });
 });
